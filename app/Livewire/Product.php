f0638747e5de7088e0182f47ac8bbcc7adf9f535
Justin Jongstra <00300061@rijnijssel.nl>
2023-11-13 16:12:49 +0100

    filters added

--- app/Livewire/Product.php
+++ app/Livewire/Product.php
@@ -9,6 +9,11 @@ class Product extends Component
 {
     public $productId;
     public $form = [];
+    public $filter = [
+        'supplier' => 'all',
+        'color' => 'all',
+        'size' => 'all',
+    ];
 
     public function mount()
     {
@@ -17,17 +22,47 @@ class Product extends Component
 
     public function render()
     {
+        $product = ProductModel::withTrashed()->find($this->productId);
+        $productSpecificationsQuery = $product->productSpecifications();
+        $productSpecifications = $productSpecificationsQuery->get();
+
+
+        if ($this->filter['supplier'] !== 'all') {
+            $productSpecificationsQuery->where('supplier_id', $this->filter['supplier']);
+        }
+
+        if ($this->filter['color'] !== 'all') {
+            $productSpecificationsQuery->where('color_id', $this->filter['color']);
+        }
+
+        if ($this->filter['size'] !== 'all') {
+            $productSpecificationsQuery->where('size_id', $this->filter['size']);
+        }
+        $productSpecifications2 = $productSpecificationsQuery->get();
+
+
+        // Get unique color, size, and supplier IDs from the product specifications
+        $colorIds = $productSpecifications->pluck('color_id')->unique()->toArray();
+        $sizeIds = $productSpecifications->pluck('size_id')->unique()->toArray();
+        $supplierIds = $productSpecifications->pluck('supplier_id')->unique()->toArray();
+
+        // Get only the relevant options based on the product specifications
+        $colors = \App\Models\Color::whereIn('id', $colorIds)->get(['id', 'key']);
+        $sizes = \App\Models\Size::whereIn('id', $sizeIds)->get(['id', 'key']);
+        $suppliers = \App\Models\Supplier::whereIn('id', $supplierIds)->get(['id', 'name']);
 
         return view('livewire.product', [
-            'productSpecifications' => ProductModel::withTrashed()->find($this->productId)->productSpecifications()->get(),
-            'product' => ProductModel::withTrashed()->find($this->productId),
-            'sizes' => \App\Models\Size::all(),
-            'colors' => \App\Models\Color::all(),
-            'suppliers' => \App\Models\Supplier::all(),
+            'productSpecifications' => $productSpecifications2,
+            'product' => $product,
+            'filter_sizes' => $sizes,
+            'filter_colors' => $colors,
+            'filter_suppliers' => $suppliers,
             'sizeGroups' => \App\Models\SizeGroup::all(),
+            'sizes' => $sizes,
+            'colors' => $colors,
+            'suppliers' => $suppliers,
         ]);
     }
-
     public function create()
     {
         $product = ProductModel::withTrashed()->find($this->productId);
--- resources/views/livewire/product.blade.php
+++ resources/views/livewire/product.blade.php
@@ -19,13 +19,13 @@
 
         <div class="flex flex-col">
             <label for="size_group" class="text-lg font-semibold mb-2">Size Group</label>
-                <select wire:model="form.size_group"
-                        class="border rounded-md p-2 mb-4 dark:bg-gray-700 dark:text-gray-200">
-                    <option value="">Select Size Group</option>
-                    @foreach ($sizeGroups as $sizeGroup)
-                        <option value="{{ $sizeGroup->id }}">{{ $sizeGroup->name }}</option>
-                    @endforeach
-                </select>
+            <select wire:model="form.size_group"
+                    class="border rounded-md p-2 mb-4 dark:bg-gray-700 dark:text-gray-200">
+                <option value="">Select Size Group</option>
+                @foreach ($sizeGroups as $sizeGroup)
+                    <option value="{{ $sizeGroup->id }}">{{ $sizeGroup->name }}</option>
+                @endforeach
+            </select>
             </label>
         </div>
 
@@ -48,32 +48,61 @@
 
         <x-button>Save</x-button>
     </form>
-        <x-section-border />
-
-        <div class="mx-4 grid grid-cols-2 gap-4">
-            @foreach($productSpecifications as $specification)
-                <div class="mb-4 border-b pb-4">
-                    <p class="text-lg font-semibold">
-                        @if (isset($specification->supplier))
-                            <span class="text-purple-500">{{ $specification->supplier->name }}</span>
-                        @endif
-                        @if (isset($specification->size))
-                            - <span class="text-blue-500">{{ $specification->size->key }}</span>
-                        @else
-                            - <span class="text-blue-500">N/A</span>
-                        @endif
-                        @if (isset($specification->color))
-                            - <span class="text-green-500">{{ $specification->color->key }}</span>
-                        @else
-                            - <span class="text-blue-500">N/A</span>
-                        @endif
-                        <x-danger-button wire:click="delete({{ $specification->id }})">Delete</x-danger-button>
-                    </p>
-                    <p class="text- {{ $specification->stock > 5 ? 'text-green-500' : 'text-red-500'}}">
-                        Stock: <span class="font-bold">{{ $specification->stock }}</span>
-                    </p>
-                </div>
+    <x-section-border/>
+    <div class="mb-4">
+        <select wire:model.live="filter.supplier"
+                class="py-2 px-3 border rounded-lg focus:outline-none focus:ring focus:border-blue-300 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-700">
+            <option value="all">All suppliers</option>
+            @foreach($filter_suppliers as $supplier)
+                <option value="{{ $supplier->id }}">{{ $supplier->name }}</option>
             @endforeach
+        </select>
+        <select wire:model.live="filter.color"
+                class="py-2 px-3 border rounded-lg focus:outline-none focus:ring focus:border-blue-300 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-700">
+            <option value="all">All Colors</option>
+            @foreach($filter_colors as $color)
+                <option value="{{ $color->id }}">{{ $color->key }}</option>
+            @endforeach
+        </select>
+
+        <select wire:model.live="filter.size"
+                class="py-2 px-3 border rounded-lg focus:outline-none focus:ring focus:border-blue-300 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-700">
+            <option value="all">All Sizes</option>
+            @foreach($filter_sizes as $size)
+                <option value="{{ $size->id }}">{{ $size->key }}</option>
+            @endforeach
+        </select>
+
+    </div>
+    @if ($productSpecifications->count() == 0)
+        <p class="text-sm font-semibold text-gray-600">
+            Nothing found.
+        </p>
+    @endif
+<div class="mx-4 grid grid-cols-2 gap-4">
+    @foreach($productSpecifications as $specification)
+        <div class="mb-4 border-b pb-4">
+            <p class="text-lg font-semibold">
+                @if (isset($specification->supplier))
+                    <span class="text-purple-500">{{ $specification->supplier->name }}</span>
+                @endif
+                @if (isset($specification->size))
+                    - <span class="text-blue-500">{{ $specification->size->key }}</span>
+                @else
+                    - <span class="text-blue-500">N/A</span>
+                @endif
+                @if (isset($specification->color))
+                    - <span class="text-green-500">{{ $specification->color->key }}</span>
+                @else
+                    - <span class="text-blue-500">N/A</span>
+                @endif
+                <x-danger-button wire:click="delete({{ $specification->id }})">Delete</x-danger-button>
+            </p>
+            <p class="text- {{ $specification->stock > 5 ? 'text-green-500' : 'text-red-500'}}">
+                Stock: <span class="font-bold">{{ $specification->stock }}</span>
+            </p>
         </div>
+    @endforeach
+</div>
 
 </div>
