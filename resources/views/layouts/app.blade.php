5364488257dd5c5e7193e169cb9f701835fb1492
Justin Jongstra <00300061@rijnijssel.nl>
2023-11-15 17:12:07 +0100

    added link-border / fix button

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,15 +33,18 @@
             <div class="min-h-screen">
                 <!-- Page Heading -->
                 @if (isset($header))
-                    <header class="bg-white dark:bg-gray-800 shadow">
-                        <div class="flex justify-end mx-8 pt-2 absolute right-7">
-                            @livewire('low-stock-notification')
-                        </div>
-                        <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                            {{ $header }}
+                    <header class="bg-white dark:bg-gray-800 shadow relative">
+                        <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8 flex justify-between items-center">
+                            <div>
+                                {{ $header }}
+                            </div>
+                            <div>
+                                @livewire('low-stock-notification')
+                            </div>
                         </div>
                     </header>
                 @endif
+
 
                 <!-- Page Content -->
                 <main class="p-4">
--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -74,7 +74,7 @@
                     {{ __('Products') }}
                 </x-nav-link>
 
-                <x-section-border/>
+                <x-link-border/>
 
                 <x-section-title :title="__('Crud')" :description="__('Manage your data.')" :class="'mx-4'"/>
                 @php $pages = ['supplier', 'color', 'size', 'sizeGroup']; @endphp
@@ -88,73 +88,13 @@
                         {{ ucwords(join(' ', preg_split('/(?=[A-Z])/', $page))) }}
                     </x-nav-link>
                 @endforeach
-                <x-section-border/>
+                <x-link-border/>
                 <x-section-title :title="__('Audit Trail')" :description="__('View all database changes.')" :class="'mx-4'"/>
                 <x-nav-link href="{{ route('audit-trail') }}" :active="request()->routeIs('audit-trail')">
                     {{ __('Audit Trail') }}
                 </x-nav-link>
             </div>
 
-        </div>
-    </div>
-
-
-    <!-- Responsive Navigation Menu -->
-    <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
-        <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
-            </x-responsive-nav-link>
-            <x-responsive-nav-link href="{{ route('products') }}" :active="request()->routeIs('products')">
-                {{ __('Products') }}
-            </x-responsive-nav-link>
-            <x-responsive-nav-link href="{{ route('crud') }}" :active="request()->routeIs('crud')">
-                {{ __('Crud') }}
-            </x-responsive-nav-link>
-            <x-responsive-nav-link href="{{ route('audit-trail') }}" :active="request()->routeIs('audit-trail')">
-                {{ __('Audit Trail') }}
-            </x-responsive-nav-link>
-        </div>
-
-        <!-- Responsive Settings Options -->
-        <div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">
-            <div class="flex items-center px-4">
-                @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
-                    <div class="shrink-0 me-3">
-                        <img class="h-10 w-10 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}"
-                             alt="{{ Auth::user()->name }}"/>
-                    </div>
-                @endif
-
-                <div>
-                    <div class="font-medium text-base text-gray-800 dark:text-gray-200">{{ Auth::user()->name }}</div>
-                    <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
-                </div>
-            </div>
-
-            <div class="mt-3 space-y-1">
-                <!-- Account Management -->
-                <x-responsive-nav-link href="{{ route('profile.show') }}" :active="request()->routeIs('profile.show')">
-                    {{ __('Profile') }}
-                </x-responsive-nav-link>
-
-                @if (Laravel\Jetstream\Jetstream::hasApiFeatures())
-                    <x-responsive-nav-link href="{{ route('api-tokens.index') }}"
-                                           :active="request()->routeIs('api-tokens.index')">
-                        {{ __('API Tokens') }}
-                    </x-responsive-nav-link>
-                @endif
-
-                <!-- Authentication -->
-                <form method="POST" action="{{ route('logout') }}" x-data>
-                    @csrf
-
-                    <x-responsive-nav-link href="{{ route('logout') }}"
-                                           @click.prevent="$root.submit();">
-                        {{ __('Log Out') }}
-                    </x-responsive-nav-link>
-                </form>
-            </div>
         </div>
     </div>
 </nav>
